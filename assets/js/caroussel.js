25585e0d19f6fe0be5d0dd509fedd0f3f971792c
LeMufas2a <codage.mufasa@gmail.com>
2021-03-17 19:06:43 +0100

    caroussel

--- assets/js/caroussel.js
+++ assets/js/caroussel.js
@@ -1,10 +1,16 @@
+export { myButton, myBilly }
 let myButton = [...document.querySelectorAll('.myButton')]
 let myBilly = document.querySelector('.myBilly')
-
 myButton.forEach(e => {
     e.addEventListener('click', (e) => {
         let i = myButton.indexOf(e.target)
-        myBilly.style.transform = `translateX(-${i * 25}%)`
         myBilly.style.transition = `1s`
+        if (window.matchMedia("(max-width: 600px)").matches) {
+            myBilly.style.transform = `translateX(-${i * 100}%)`
+        } else {
+            myBilly.style.transform = `translateX(-${i * 25}%)`
+        }
     })
 });
+
+
